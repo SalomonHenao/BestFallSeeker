926a29813b0569a5cff6d330ae874b9a0b6880e3
Salomon <shenao@identidadtech.com>
2022-01-08 02:41:18 -0500

    Add project files.

---
+++ BestFallSeeker/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestFallSeeker
+{
+    class Program
+    {
+        //Main script
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Setup variables
+                string filePath = @"C:\map.txt";
+                char separator = ' ';
+
+                //Process
+
+                //Create stopwatch to control elapsed time
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                Console.WriteLine("Process started.\nPlease wait...");
+
+                //Loads raw data to memory
+                List<List<int>> mountainMap = GetFileData(filePath, separator);
+
+                //Calls best fall calculator and stops timer after it returns data
+                List<Coordinate> bestFall = CalculateBestFall(mountainMap);
+                timer.Stop();
+
+                //Print results
+                if (bestFall != null)
+                {
+                    //Titles
+                    Console.WriteLine("\nBest fall found:");
+                    Console.WriteLine($"Elapsed: {Math.Round(timer.Elapsed.TotalSeconds, 2)} seconds");
+                    Console.WriteLine($"Steps: {bestFall.Count}");
+                    Console.WriteLine($"Drop: {GetMapValue(bestFall.First(), mountainMap) - GetMapValue(bestFall.Last(), mountainMap)}");
+                    Console.WriteLine($"\nCoordinates:");
+
+                    //Steps
+                    for (int i = 0; i < bestFall.Count; i++)
+                    {
+                        string lineIndicator = i == 0 ? "Start"
+                            : i == (bestFall.Count-1) ? "Finally"
+                            : $"Step {i}";
+
+                        Console.WriteLine($"{lineIndicator}: X{bestFall[i].row},Y{bestFall[i].col}: {GetMapValue(bestFall[i], mountainMap)}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"There seems to be an issue with the provided data.\nNo valid falls were found.");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("\nPlease check the input dataset and the script setup.");
+                Console.WriteLine($"Error details: {ex.Message}");
+            }
+
+        }
+
+        //Loads file to memory
+        //Used a list of lists to simplify the data handling
+        private static List<List<int>> GetFileData(
+            string filePath,
+            char separator)
+        {
+            List<List<int>> fileData = new List<List<int>>();
+            
+            //Reads all bytes and uses stream reader to load data into object
+            byte[] byteArray = File.ReadAllBytes(filePath);
+            using (var sreader = new StreamReader(new MemoryStream(byteArray)))
+            {
+                //Reads first row to get size
+                List<int> size = new List<int>();
+                try
+                {
+                    //Reades the size headers and casts them to int
+                    size = sreader.ReadLine().Split(separator).Select(s => Convert.ToInt32(s)).ToList();
+                }
+                catch
+                {
+                    throw new Exception("\nThere is a problem with the size indication.\nThe first two columns of the first row must contain the matrix size.");
+                }
+
+                //Reads all the other rows
+                while (!sreader.EndOfStream)
+                {
+                    List<int> row = sreader.ReadLine().Split(separator).Select(s => Convert.ToInt32(s)).ToList();
+                    fileData.Add(row);
+                }
+
+                //Validates the headers and the data dimenssions by using them
+                if(size.Count != 2
+                    || fileData.Count != size.First()
+                    || fileData.OrderBy(o => o.Count).First().Count != size.Last()
+                    || fileData.OrderBy(o => o.Count).Last().Count != size.Last())
+                {
+                    throw new Exception("Data doesn't match with the given size.");
+                }
+            }
+            return fileData;
+        }
+
+        //Calculates the best fall
+        //The longest and tallest
+        private static List<Coordinate> CalculateBestFall(
+            List<List<int>> mountainMap)
+        {
+            //Creates bag designed to store data incoming from parallel treads
+            //All the feasible routes will be stored here during the analysis process
+            ConcurrentBag<List<Coordinate>> feasiblePaths = new ConcurrentBag<List<Coordinate>>();
+            
+            //Max parallelism of 10 threads
+            ParallelOptions parallelismLimit = new ParallelOptions { MaxDegreeOfParallelism = 10 };
+
+            //As we have two for loops, we'll examine 100 possible landing points at a time
+            //The system checks all the possible landing points and their derivated routes
+            Parallel.For(0, mountainMap.Count, parallelismLimit, rows =>
+            {
+                Parallel.For(0, mountainMap.First().Count, parallelismLimit, cols =>
+               {
+                   //Creates the first coordinate, the landing spot
+                   Coordinate landingSite = new Coordinate { row = rows, col = cols };
+                   //Starts an exploring tree that will follow all the possible routes
+                   GetfeasiblePaths(new List<Coordinate>(), landingSite, mountainMap, feasiblePaths);
+               });
+            });
+
+            //Orders the concurrent bag and extracts the longest and tallest route
+            List<Coordinate> bestFall = feasiblePaths
+                        .OrderByDescending(o => o.Count)
+                        .ThenByDescending(or =>
+                            GetMapValue(or.First(), mountainMap)
+                            -
+                            GetMapValue(or.Last(), mountainMap))
+                        .FirstOrDefault();
+
+            return bestFall;
+        }
+
+        //Creates an exploring tree
+        //Creates all the possible routes starting from a position
+        private static void GetfeasiblePaths(
+            List<Coordinate> history,
+            Coordinate currentPosition,
+            List<List<int>> mountainMap,
+            ConcurrentBag<List<Coordinate>> feasiblePaths)
+        {
+            //History contains all the steps that a thread has followed since its beginin
+            history = history.Select(s => new Coordinate { row = s.row, col = s.col }).ToList();
+
+            //Every new call stores a new coordinate in a thread history
+            //This enables the recopilation of all the feasible routes
+            history.Add(new Coordinate { row = currentPosition.row, col = currentPosition.col });
+
+            //If a thread arrives to a coordinate without an exit, it's time to store the route
+            //If not, the thread must do another iteration over every available path to follow
+            if (ArrivedToEnd(history, mountainMap))
+            {
+                //Adds the thread history to the feasible paths concurrent bag
+                feasiblePaths.Add(history); 
+            }
+            else
+            {
+                //Gets all the available paths to follow from the current coordinate
+                List<Coordinate> availablePaths = GetAvailablePaths(currentPosition, mountainMap);
+
+                //Creates a new iteration with every available path to follow
+                foreach (Coordinate path in availablePaths)
+                {
+                    GetfeasiblePaths(history, path, mountainMap, feasiblePaths);
+                }
+            }
+        }
+
+        //Creates the available paths to follow from a given position
+        private static List<Coordinate> GetAvailablePaths(
+            Coordinate currentPosition,
+            List<List<int>> mountainMap)
+        {
+            List<Coordinate> availablePaths = new List<Coordinate>();
+            
+            //Calculates left path
+            availablePaths.Add(new Coordinate
+            {
+                row = currentPosition.row,
+                col = currentPosition.col - 1
+            });
+
+            //Calculates right path
+            availablePaths.Add(new Coordinate
+            {
+                row = currentPosition.row,
+                col = currentPosition.col + 1
+            });
+
+            //Calculates up path
+            availablePaths.Add(new Coordinate
+            {
+                row = currentPosition.row - 1,
+                col = currentPosition.col
+            });
+
+            //Calculates down path
+            availablePaths.Add(new Coordinate
+            {
+                row = currentPosition.row + 1,
+                col = currentPosition.col
+            });
+
+            //Filters the paths to allow only those matching the rules
+            //1. Can't exceed the matrix dimenssion
+            //2. The current position value must be grather than the next position value
+            availablePaths = availablePaths
+                            .Where(w =>
+                                w.row >= 0
+                                && w.col >= 0
+                                && w.row <= (mountainMap.Count() -1)
+                                && w.col <= (mountainMap.First().Count() -1)
+                                && GetMapValue(currentPosition, mountainMap) > GetMapValue(w, mountainMap))
+                            .ToList();
+
+            return availablePaths;
+        }
+
+        //Validates if no available paths for a given position
+        private static bool ArrivedToEnd(
+            List<Coordinate> history,
+            List<List<int>> mountainMap)
+        {
+            List<Coordinate> pathOptions = GetAvailablePaths(history.Last(), mountainMap);
+            if(pathOptions.Count() > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        //Extracts a cell value from the map using XY coordinates
+        private static int GetMapValue(
+            Coordinate position,
+            List<List<int>> mountainMap)
+        {
+            return mountainMap[position.row][position.col];
+        }
+
+        //Class used to navigate the matrix with XY coordinates
+        private class Coordinate
+        {
+            public int row { get; set; }
+
+            public int col { get; set; }
+        }
+    }
+}
