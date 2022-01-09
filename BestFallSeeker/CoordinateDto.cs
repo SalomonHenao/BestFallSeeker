ce29645b5ee5448ce230137265957842e908b32a
Salomon <shenao@identidadtech.com>
2022-01-08 21:00:59 -0500

    re ordered files and fixed redaction

---
+++ BestFallSeeker/CoordinateDto.cs
@@ -0,0 +1,11 @@
+﻿
+//Class used to navigate the matrix with XY coordinates
+namespace BestFallSeeker
+{
+    public class CoordinateDto
+    {
+        public int row { get; set; }
+
+        public int col { get; set; }
+    }
+}
